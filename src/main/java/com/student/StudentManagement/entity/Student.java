bc13b86bf1404bbf521de62c9802d09247fa1b1d
kumar-animesh28 <kumaranimesh0228@gmail.com>
2025-10-04 00:13:29 +0530

    Sprintboot project

--- src/main/java/com/student/StudentManagement/entity/Student.java
+++ src/main/java/com/student/StudentManagement/entity/Student.java
@@ -1,9 +1,6 @@
 package com.student.StudentManagement.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
